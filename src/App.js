eeade4235da5ead9b14dd153085b29965cdd4cd9
Avinash Pawar <cloudmail.project.x@gmail.com>
2021-07-20 20:54:10 +0530

    style: add new UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import home from "./pages/home";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+export default App;
