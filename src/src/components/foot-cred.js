31c1c00f52d58a80b5ca7a19d31f12d622ac8318
vishwajeet <cloudmail.vishwajeet@gmail.com>
2021-07-20 22:03:53 +0530

    style: Add new UI

---
+++ src/src/components/foot-cred.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+
+class Footer_credit extends Component {
+  render() {
+    return (
+      <footer id="footer">
+        <div className="cred disable-select">
+          Connect. <span> Code. </span> Execute.
+        </div>
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-info">
+                  <h3>
+                    <a className="splogo" href="index.html">
+                      &lt;/&gt;
+                    </a>
+                    <a className="sp" href="index.html">
+                      CodeSpace
+                    </a>
+                  </h3>
+                  <h4 className="sp">
+                    Connect.<span style={{ color: "#0761d1" }}> Code.</span>
+                    Execute.
+                  </h4>
+                  <div className="social-links mt-5">
+                    <a href className="github">
+                      <i className="fa fa-github large fa-3x" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Made With</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right" /> <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">About us</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Services</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Terms of service</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Privacy policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Links</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+
+                    <a href="#">Web Design</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Web Development</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Product Management</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+
+                    <a href="#">Marketing</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right" />
+                    <a href="#">Graphic Design</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <h4>Contact Us</h4>
+                <p>
+                  Kolhapur <br />
+                  India <br />
+                  <br />
+                  <strong>Phone:</strong> 0000000000 <br />
+                  <strong>Email:</strong> codespacedot@gmail.com
+                  <br />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="copyright">
+            © Copyright
+            <strong>
+              <span>CodeSpace</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+          <div className="credits">
+            Designed by <a href>CodeSpace</a>
+          </div>
+        </div>
+        <div>
+          <div id="preloader">
+            <span> &lt;/&gt; </span>
+          </div>
+          <a
+            href="#"
+            className="back-to-top d-flex align-items-center justify-content-center"
+          >
+            <i className="fa fa-arrow-up" />
+          </a>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer_credit;
