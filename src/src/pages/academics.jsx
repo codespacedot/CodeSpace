67dace6de2bdc96ef3a5b41353bc4e9cd5724580
kalyanihendra <kalyanihpawar@gmail.com>
2021-07-24 23:55:15 -0400

    fix: fix preloader issue and remove header from it

--- src/src/pages/academics.jsx
+++ src/src/pages/academics.jsx
@@ -56,166 +56,168 @@ function Academics() {
   return (
     <div>
       <Online>
-        <Header />
-        {year === 0 ? (
-          (window.location = "/")
-        ) : isLoading ? (
+        {/* aplying loader logic here ,either we receiving data or not*/}
+        {isLoading ? (
           <div>
             <Preloader />
           </div>
         ) : (
-          <section class="card" id="year">
-            <div class="container  align-items-center ">
-              <div class="row justify-content-center heading border-bottom">
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
-                  <h1 class="sp">{yearName}</h1>
-                </div>
-              </div>
-
-              <div class="row ">
-                <div
-                  class="row justify-content-center heading "
-                  style={{ marginLeft: "0px", marginRight: "0px" }}
-                >
+          // calling body of academics page
+          <div>
+            <Header />
+            <section class="card" id="year">
+              <div class="container  align-items-center ">
+                <div class="row justify-content-center heading border-bottom">
                   <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
-                    <h2 class="sp">Semister {oddSem}</h2>
+                    <h1 class="sp">{yearName}</h1>
                   </div>
                 </div>
 
-                {yearData.ODD_SEMESTER.SUBJECTS.map((odd) => {
-                  return (
-                    <div
-                      key={odd.key}
-                      class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
-                    >
-                      <div class="icon-box">
-                        <div class="icon">
-                          {" "}
-                          <h4>
-                            <a href="/">{odd.abbreviation}</a>
-                          </h4>{" "}
-                        </div>
-
-                        <div class="none">
-                          {" "}
-                          <p>
-                            <a href="/">{odd.name}</a>
-                          </p>
-                        </div>
-                      </div>
+                <div class="row ">
+                  <div
+                    class="row justify-content-center heading "
+                    style={{ marginLeft: "0px", marginRight: "0px" }}
+                  >
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
+                      <h2 class="sp">Semister {oddSem}</h2>
                     </div>
-                  );
-                })}
-                {yearData.ODD_SEMESTER.LABS.map((odd) => {
-                  return (
-                    <div
-                      key={odd.key}
-                      class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
-                    >
-                      <div class="icon-box">
-                        <div class="icon">
-                          {" "}
-                          <h4>
-                            <a
-                              href={gitLabLink + odd.abbreviation}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {odd.abbreviation} Lab
-                            </a>
-                          </h4>{" "}
-                        </div>
-
-                        <div class="none">
-                          {" "}
-                          <p>
-                            <a
-                              href={gitLabLink + odd.abbreviation}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {odd.name}
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-
-                <div
-                  class="row justify-content-center heading "
-                  style={{ marginLeft: "0px", marginRight: "0px" }}
-                >
-                  <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
-                    <h2 class="sp">Semister {evenSem}</h2>
                   </div>
+
+                  {yearData.ODD_SEMESTER.SUBJECTS.map((odd) => {
+                    return (
+                      <div
+                        key={odd.key}
+                        class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
+                      >
+                        <div class="icon-box">
+                          <div class="icon">
+                            {" "}
+                            <h4>
+                              <a href="/">{odd.abbreviation}</a>
+                            </h4>{" "}
+                          </div>
+
+                          <div class="none">
+                            {" "}
+                            <p>
+                              <a href="/">{odd.name}</a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                  {yearData.ODD_SEMESTER.LABS.map((odd) => {
+                    return (
+                      <div
+                        key={odd.key}
+                        class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
+                      >
+                        <div class="icon-box">
+                          <div class="icon">
+                            {" "}
+                            <h4>
+                              <a
+                                href={gitLabLink + odd.abbreviation}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {odd.abbreviation} Lab
+                              </a>
+                            </h4>{" "}
+                          </div>
+
+                          <div class="none">
+                            {" "}
+                            <p>
+                              <a
+                                href={gitLabLink + odd.abbreviation}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {odd.name}
+                              </a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+
+                  <div
+                    class="row justify-content-center heading "
+                    style={{ marginLeft: "0px", marginRight: "0px" }}
+                  >
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
+                      <h2 class="sp">Semister {evenSem}</h2>
+                    </div>
+                  </div>
+
+                  {yearData.EVEN_SEMESTER.SUBJECTS.map((even) => {
+                    return (
+                      <div
+                        key={even.key}
+                        class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
+                      >
+                        <div class="icon-box">
+                          <div class="icon">
+                            {" "}
+                            <h4>
+                              <a href="/">{even.abbreviation}</a>
+                            </h4>{" "}
+                          </div>
+
+                          <div class="none">
+                            {" "}
+                            <p>
+                              <a href="/">{even.name}</a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+
+                  {yearData.EVEN_SEMESTER.LABS.map((even) => {
+                    return (
+                      <div
+                        key={even.key}
+                        class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
+                      >
+                        <div class="icon-box">
+                          <div class="icon">
+                            {" "}
+                            <h4>
+                              <a
+                                href={gitLabLink + even.abbreviation}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {even.abbreviation} Lab
+                              </a>
+                            </h4>{" "}
+                          </div>
+
+                          <div class="none">
+                            {" "}
+                            <p>
+                              <a
+                                href={gitLabLink + even.abbreviation}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {even.name}
+                              </a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
-
-                {yearData.EVEN_SEMESTER.SUBJECTS.map((even) => {
-                  return (
-                    <div
-                      key={even.key}
-                      class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
-                    >
-                      <div class="icon-box">
-                        <div class="icon">
-                          {" "}
-                          <h4>
-                            <a href="/">{even.abbreviation}</a>
-                          </h4>{" "}
-                        </div>
-
-                        <div class="none">
-                          {" "}
-                          <p>
-                            <a href="/">{even.name}</a>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-
-                {yearData.EVEN_SEMESTER.LABS.map((even) => {
-                  return (
-                    <div
-                      key={even.key}
-                      class="col-lg-4 col-md-4 d-flex align-items-stretch  cardspace"
-                    >
-                      <div class="icon-box">
-                        <div class="icon">
-                          {" "}
-                          <h4>
-                            <a
-                              href={gitLabLink + even.abbreviation}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {even.abbreviation} Lab
-                            </a>
-                          </h4>{" "}
-                        </div>
-
-                        <div class="none">
-                          {" "}
-                          <p>
-                            <a
-                              href={gitLabLink + even.abbreviation}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              {even.name}
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         )}
         {/* calling footer component from src-components-footer.js */}
         <FooterCred />
